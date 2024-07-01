36d784275540f9c680a2208a903b836397b725db
Anthony Pereira <101676526+Clarensedor@users.noreply.github.com>
2024-06-30 23:14:17 -0300

    post y delete

--- api/index.ts
+++ api/index.ts
@@ -14,3 +14,42 @@ export const getJokes = async () => {
   }
 };
 
+export const addJoke = async (newJoke) => {
+  const url = 'http://10.0.2.2:3000/api/tasks';
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newJoke),
+    });
+    if (response.ok) {
+      const payload = await response.json();
+      return payload;
+    } else {
+      console.error('error');
+    }
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+export const deleteJoke = async (taskId) => {
+  const url = `http://10.0.2.2:3000/api/tasks/${taskId}`;
+  try {
+    const response = await fetch(url, {
+      method: 'DELETE',
+    });
+    if (response.ok) {
+      return true;
+    } else {
+      console.error('An error occurred while deleting the joke');
+      return false;
+    }
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+};
--- screens/DetailsScreen/index.tsx
+++ screens/DetailsScreen/index.tsx
@@ -1,13 +1,41 @@
-import React from 'react';
-import {Text, StyleSheet, View} from 'react-native';
+import React, { useState } from 'react';
+import { Text, StyleSheet, View, Button, ActivityIndicator } from 'react-native';
+import { deleteJoke } from '../../api'; // Importa la función de eliminación de la API
 
-const DetailsScreen = ({route}) => {
+const DetailsScreen = ({ route, navigation }) => {
   const info = route.params.item;
+  const { deleteLocalJoke } = route.params; // Recibe la función de eliminación local
+  const [loading, setLoading] = useState(false);
+
+  const handleDelete = async () => {
+    setLoading(true);
+    const success = await deleteJoke(info.id);
+    if (success) {
+      deleteLocalJoke(info.id); // Elimina la tarea del estado local
+      setLoading(false);
+      navigation.goBack(); // Navega de regreso a la pantalla principal
+    } else {
+      setLoading(false);
+      // Manejar el error según sea necesario
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.h1}>Details Screen</Text>
+      <Text>{info.id}</Text>
       <Text>{info.title}</Text>
       <Text>{info.description}</Text>
+      <Text>{info.assignedTo}</Text>
+      <Text>{info.startDate}</Text>
+      <Text>{info.status}</Text>
+      <Text>{info.endDate}</Text>
+      <Text>{info.priority}</Text>
+      {loading ? (
+        <ActivityIndicator size="large" color="#0000ff" />
+      ) : (
+        <Button title="Delete" onPress={handleDelete} />
+      )}
     </View>
   );
 };
